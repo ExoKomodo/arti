31f5c6d610bd9758cee9f458b072a767990cd18a
James Orson <jamesaorson@gmail.com>
2022-12-20 07:04:11 -0800

    Add reduce as specialization of FoldRight

--- backend/lib/functional/fold.go
+++ backend/lib/functional/fold.go
@@ -1,5 +1,9 @@
 package functional
 
+func Reduce[T, U any](collection []T, reducer func(T, U) U, accumulator U) U {
+	return FoldRight(collection, reducer, accumulator)
+}
+
 func FoldRight[T, U any](collection []T, reducer func(T, U) U, accumulator U) U {
 	for _, element := range collection {
 		accumulator = reducer(element, accumulator)
