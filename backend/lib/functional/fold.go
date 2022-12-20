53ec4f9a2da4724c8e5ed10c7b1df89679af062b
James Orson <jamesaorson@gmail.com>
2022-12-20 07:03:35 -0800

    Switch reduce to fold

---
+++ backend/lib/functional/fold.go
@@ -0,0 +1,18 @@
+package functional
+
+func FoldRight[T, U any](collection []T, reducer func(T, U) U, accumulator U) U {
+	for _, element := range collection {
+		accumulator = reducer(element, accumulator)
+	}
+
+	return accumulator
+}
+
+func FoldLeft[T, U any](collection []T, reducer func(T, U) U, accumulator U) U {
+	for i := len(collection) - 1; i >= 0; i-- {
+		element := collection[i]
+		accumulator = reducer(element, accumulator)
+	}
+
+	return accumulator
+}
--- backend/lib/functional/reduce.go
+++
@@ -1,9 +0,0 @@
-package functional
-
-func Reduce[T, U any](collection []T, reducer func(T, U) U, accumulator U) U {
-	for _, element := range collection {
-		accumulator = reducer(element, accumulator)
-	}
-
-	return accumulator
-}
