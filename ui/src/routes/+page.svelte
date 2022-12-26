3a30c940ea70834fa01028cb9729c5c923af9d29
Sašo Skube <saso.skube@gmail.com>
2022-12-26 21:49:13 +0100

    Show services in UI

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,2 +1,15 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<ul>
+<li><a href="localhost:9001">S3 backend</a></li>
+<li><a href="http://localhost:5500/">Docker registry</a></li>
+<li><a href="localhost:8088">nginx proxy for many things</a></li>
+</ul>
+
+Using nginx:
+<ul>
+<li><a href="http://localhost:8088/s3">S3 backend</a></li>
+<li><a href="http://localhost:8088/registry">Docker registry</a></li>
+<li><a href="http://localhost:8088">this page</a></li>
+</ul>
