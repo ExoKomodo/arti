a23f28b039cf733b1ea8e54e752e3591a93123a2
James Orson <jamesaorson@gmail.com>
2023-01-06 06:31:44 -0800

    Add back Reduce

--- backend/lib/functional/fold.go
+++ backend/lib/functional/fold.go
@@ -16,3 +16,15 @@ func FoldRight[T any](collection []T, reducer func(T, T) T, accumulator T) T {
 
 	return accumulator
 }
+
+func Reduce[T any](collection []T, reducer func(T, T) T) T {
+	length := len(collection)
+	if length == 0 {
+		var default T
+		return default
+	}
+	if length == 1 {
+		return collection[0]
+	}
+	return Fold(collection[:length-1], reducer, collection[length-1])
+}
